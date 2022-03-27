3d881715421d1ab5caf6ba0652a66677d7f5d170
egoing <egoing@gmail.com>
2022-03-27 15:05:31 +0900

    5. 홈페이지 구현

--- server.py
+++ server.py
@@ -4,9 +4,30 @@ import random
 app = Flask(__name__)
 
 
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascruot', 'body': 'javascript is ...'}
+]
+
+
 @app.route('/')
 def index():
-    return 'Welcome'
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>
+    '''
 
 
 @app.route('/create/')
@@ -14,7 +35,7 @@ def create():
     return 'Create'
 
 
-@app.route('/read/<id:int>/')
+@app.route('/read/<id>/')
 def read(id):
     print(id)
     return 'Read '+id
