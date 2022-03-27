ac8eb60d93556f3b76ff52e273ba89b07917c131
egoing <egoing@gmail.com>
2022-03-27 22:05:37 +0900

    8.수정

--- server.py
+++ server.py
@@ -11,7 +11,12 @@ topics = [
 ]
 
 
-def template(contents, content):
+def template(contents, content, id=None):
+    contextUI = ''
+    if id != None:
+        contextUI = f'''
+            <li><a href="/update/{id}/">update</a></li>
+        '''
     return f'''<!doctype html>
     <html>
         <body>
@@ -22,6 +27,7 @@ def template(contents, content):
             {content}
             <ul>
                 <li><a href="/create/">create</a></li>
+                {contextUI}
             </ul>
         </body>
     </html>
@@ -49,7 +55,7 @@ def read(id):
             title = topic['title']
             body = topic['body']
             break
-    return template(getContents(), f'<h2>{title}</h2>{body}')
+    return template(getContents(), f'<h2>{title}</h2>{body}', id)
 
 
 @app.route('/create/', methods=['GET', 'POST'])
@@ -73,5 +79,35 @@ def create():
         nextId = nextId + 1
         return redirect(url)
 
+
+@app.route('/update/<int:id>/', methods=['GET', 'POST'])
+def update(id):
+    if request.method == 'GET': 
+        title = ''
+        body = ''
+        for topic in topics:
+            if id == topic['id']:
+                title = topic['title']
+                body = topic['body']
+                break
+        content = f'''
+            <form action="/update/{id}/" method="POST">
+                <p><input type="text" name="title" placeholder="title" value="{title}"></p>
+                <p><textarea name="body" placeholder="body">{body}</textarea></p>
+                <p><input type="submit" value="update"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        for topic in topics:
+            if id == topic['id']:
+                topic['title'] = title
+                topic['body'] = body
+                break
+        url = '/read/'+str(id)+'/'
+        return redirect(url)
 
 app.run(debug=True)
