2fdad245aa9be6bdcdcd3be0dd80fc19437fdfb2
egoing <egoing@gmail.com>
2022-03-27 13:52:57 +0900

    4. 라우팅

--- server.py
+++ server.py
@@ -6,7 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'random : <strong>'+str(random.random())+'</strong>'
+    return 'Welcome'
+
+
+@app.route('/create/')
+def create():
+    return 'Create'
+
+
+@app.route('/read/<id:int>/')
+def read(id):
+    print(id)
+    return 'Read '+id
 
 
 app.run(debug=True)
